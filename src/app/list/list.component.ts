05ff95c9aaec480c65968ecebdd1412818a26be9
Dariusz Dajcz <daarek.dajcz@gmail.com>
2021-03-03 23:23:54 +0100

    Finish eToDoList

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -8,14 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
 
   newTask: string;
-
   taskList: Array<string> = [];
+  taskDone: Array<string> = [];
 
   add() {
     this.taskList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.taskList);
   }
 
+  remove(task: string) {
 
+    this.taskList = this.taskList.filter(e => e !== task);
+
+  }
+
+  done(task: string) {
+
+    this.taskDone.push(task);
+    this.remove(task)
+    console.log(this.taskDone)
+
+  }
 
 
   constructor() { }
